f2ff46c2275d51ec300bc1c0e2e00f5e89c284b5
odiinnn <msinkevic103@gmail.com>
2023-10-07 11:57:37 +0300

    update fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import { ReactNode } from 'react';
 import { AppProps } from 'next/app';
+import { Orbitron } from 'next/font/google';
 
 import '../styles/global.css';
 
+// eslint-disable-next-line new-cap
+const orbitron = Orbitron({ weight: '400', subsets: ['latin'] });
+
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
-  return <Component {...pageProps} />;
+  return (
+    <main className={orbitron.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
